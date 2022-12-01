618cd979f629ef81bfc5f56e851c457ffacfd942
iqubic <avi.caspe@gmail.com>
2022-11-30 21:28:19 -0800

    Day 1 solved

--- src/Day01.hs
+++ src/Day01.hs
@@ -2,15 +2,30 @@ module Day01 where
 
 import Common.Runner
 import Common.Parser
+import Data.List (sortOn)
+import Data.Ord (Down(..))
 
-part1 :: String -> a
-part1 i = undefined
+-- For each reindeer, sum the number of calories
+-- then take the maximum.
+part1 :: String -> Int
+part1 = maximum
+      . map sum
+      . parseInput
 
-part2 :: String -> a
-part2 i = undefined
+-- For each reindeer, sum the number of calories
+-- then sortOn Down to get the largest first,
+-- then sum the first three
+part2 :: String -> Int
+part2 = sum
+      . take 3
+      . sortOn Down
+      . map sum
+      . parseInput
 
-parseInput :: String -> [Int]
-parseInput = parseLines number
+parseInput :: String -> [[Int]]
+parseInput = parseAll $ pElf `sepBy1` eol
+  where
+    pElf = number `endBy1` eol
 
 solve :: Show a => (String -> a) -> IO (Either AoCError a)
 solve = runSolutionOnInput 1
