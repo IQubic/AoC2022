21c3c066b5e412ec7208e51d46cc92ada781797a
iqubic <avi.caspe@gmail.com>
2022-12-03 21:15:16 -0800

    Day 4 solved

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,16 +1,36 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TupleSections #-}
+
 module Day04 where
 
 import Common.Runner
 import Common.Parser
+import Common.Util (countIf)
+import Data.IntSet (IntSet)
+import Data.IntSet qualified as IS
 
-part1 :: String -> a
-part1 i = undefined
 
-part2 :: String -> a
-part2 i = undefined
+part1 :: String -> Int
+part1 = countIf (uncurry valid)
+      . parseInput
+  where
+    valid r1 r2 = r1 `IS.isSubsetOf` r2 || r2 `IS.isSubsetOf` r1
 
-parseInput :: String -> [a]
-parseInput = parseLines undefined
+part2 :: String -> Int
+part2 = countIf (\(x, y) -> not $ IS.disjoint x y)
+      . parseInput
+
+parseInput :: String -> [(IntSet, IntSet)]
+parseInput = parseLines $ do
+  first <- pRange <* char ','
+  second <- pRange
+  pure (first, second)
+    where
+      pRange :: Parser IntSet
+      pRange = do
+        first <- number <* char '-'
+        second <- number
+        pure $ IS.fromList [first..second]
 
 solve :: Show a => (String -> a) -> IO (Either AoCError a)
 solve = runSolutionOnInput 4
